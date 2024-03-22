593044f070ff6f9c8139ee1421330f82f3a4e185
hirenkuvariya09 <hirenkuvariya09@gmail.com>
2024-03-22 13:22:30 +0530

    more

--- src/assets/BlogDetails.jsx
+++ src/assets/BlogDetails.jsx
@@ -3,15 +3,17 @@ import './BlogDetails.css';
 
 function BlogDetails({ blog }) {
   return (
- 
     <div className='main'>   
-    <h2 className='blogtitle'>{blog.title}</h2>
-        {blog.image && <img className='im' src={blog.image} alt={blog.title} />}
+     <div className="content">
+          <h1>single Blogs</h1>
+          <h1>single Blogs</h1>
+        </div>
+
+      <h2 className='blogtitle'>{blog.title}</h2>
+      {blog.image && <img className='im' src={blog.image} alt={blog.title} />}
       <h4 className='Blogdes'>{blog.description}</h4>
       <p className='Blogcon'>{blog.content}</p>
-   
     </div>
-  
   );
 }
 
--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -39,25 +39,85 @@ function Home() {
 
   return (
     <div>
+     
+
+
+
       <div className='main2'>
         <div className='avtar'>
           <img src="./pngtree-3d-character-illustrator-work-on-table-and-laptop-png-image_11596148.png" id='ig1' alt="" />
         </div>
         <div class="cube-container">
           <div class="cube">
-            <div class="face front">Html</div>
-            <div class="face back">Css</div>
-            <div class="face right">React JS</div>
-            <div class="face left">JavaScript</div>
-            <div class="face top">Node JS</div>
-            <div class="face bottom">mongoDB</div>
+            <div class="face front"><img src="./html.png"  alt="" />Html</div>
+            <div class="face back"><img src="./mongodb.png"  alt="" />mongodb</div>
+            <div class="face right"><img src="./react.png"  alt="" />React JS</div>
+            <div class="face left"><img src="./javascript.png"  alt="" />JavaScript</div>
+            <div class="face top"><img src="./node.png"  alt="" />Node JS</div>
+            <div class="face bottom"> <img src="./nextjs.png"  alt="" />Next JS</div>
           </div>
         </div>
       </div>
 
-      <div className='bgimg'>
-        <img src="./map_dark_image.png" id='ig' alt="" />
+
+      <section class="do_section layout_padding">
+    <div class="container">
+    <div class="heading_container">
+        <h2>
+          we are provide 
+        </h2>
+        <p>
+            
+        </p>
       </div>
+      <div class="do_container">
+        <div class="box arrow-start arrow_bg">
+          <div class="img-box">
+            <img src="./d-1.png" alt=""/>
+          </div>
+          <div class="detail-box">
+            <h6>
+              designing
+            </h6>
+          </div>
+        </div>
+        <div class="box arrow-middle arrow_bg">
+          <div class="img-box">
+            <img src="./d-2.png" alt=""/>
+          </div>
+          <div class="detail-box">
+            <h6>
+              Development
+            </h6>
+          </div>
+        </div>
+        <div class="box arrow-middle arrow_bg">
+          <div class="img-box">
+            <img src="./d-3.png" alt=""/>
+          </div>
+          <div class="detail-box">
+            <h6>
+              Html5
+            </h6>
+          </div>
+        </div>
+        <div class="box arrow-end arrow_bg">
+          <div class="img-box">
+            <img src="./d-4.png" alt=""/>
+          </div>
+          <div class="detail-box">
+            <h6>
+              Css
+            </h6>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+
+
+     
 
 
 
@@ -83,6 +143,13 @@ function Home() {
 
       {selectedBlog && <BlogDetails blog={selectedBlog} />}
       </div>
+
+      <div className='bgimg'>
+        <img src="./map_dark_image.png" id='ig' alt="" />
+      </div>
+
+
+
     </div>
   );
 }
